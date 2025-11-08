0fe45b78085cf14a89dd8d066b5007c445b93879
Luis Vallejos <vallejosevangelistaluis@gmail.com>
2025-11-08 12:24:31 -0500

    feat(model): agregar entidad StaticAbilityData con soporte JPA y Lombok
    - Se convierte la clase StaticAbilityData en una entidad JPA con anotaciones @Entity y @Table.
    - Se agregan campos id, name y description con sus respectivas configuraciones de columna.
    - Se implementa Serializable para compatibilidad con la persistencia.
    - Se integran anotaciones de Lombok (@Getter, @Setter, @Builder, etc.) para reducir código repetitivo.

--- src/main/java/com/pokemon/game/model/StaticAbilityData.java
+++ src/main/java/com/pokemon/game/model/StaticAbilityData.java
@@ -1,9 +1,44 @@
 package com.pokemon.game.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /**
  *
  * @author Luis
  */
-public class StaticAbilityData {
-    
+@Entity
+@Table(name = "static_ability_data", schema = "pokemon_game_db")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class StaticAbilityData implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "name", unique = true, nullable = false)
+    private String name;
+
+    @Column(name = "description", nullable = false, length = 1024)
+    private String description;
 }
