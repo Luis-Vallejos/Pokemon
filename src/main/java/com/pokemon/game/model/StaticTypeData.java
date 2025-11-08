7d4d9c44328382a2f9b98a69c7f7ed50701877e9
Luis Vallejos <vallejosevangelistaluis@gmail.com>
2025-11-08 12:25:43 -0500

    feat(model): agregar entidad StaticTypeData con relaciones ManyToMany y soporte JPA/Lombok
    - Se convierte la clase StaticTypeData en una entidad JPA con anotaciones @Entity y @Table.
    - Se agregan campos id y name con sus configuraciones de columna.
    - Se implementan relaciones ManyToMany para representar las tablas intermedias:
      - type_double_damage_from
      - type_half_damage_from
      - type_no_damage_from
    - Se utiliza @Builder.Default y HashSet para inicializar las colecciones por defecto.
    - Se implementa Serializable para compatibilidad con la persistencia.
    - Se integran anotaciones de Lombok (@Getter, @Setter, @Builder, etc.) para simplificar el código.

--- src/main/java/com/pokemon/game/model/StaticTypeData.java
+++ src/main/java/com/pokemon/game/model/StaticTypeData.java
@@ -1,9 +1,61 @@
 package com.pokemon.game.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 /**
  *
  * @author Luis
  */
-public class StaticTypeData {
-    
+@Entity
+@Table(name = "static_type_data", schema = "pokemon_game_db")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class StaticTypeData implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "name", unique = true, nullable = false)
+    private String name;
+
+    // Conexioes
+    @Builder.Default
+    @ManyToMany
+    @JoinTable(name = "type_double_damage_from")
+    private Set<StaticTypeData> doubleDamageFrom = new HashSet<>();
+
+    @Builder.Default
+    @ManyToMany
+    @JoinTable(name = "type_half_damage_from")
+    private Set<StaticTypeData> halfDamageFrom = new HashSet<>();
+
+    @Builder.Default
+    @ManyToMany
+    @JoinTable(name = "type_no_damage_from")
+    private Set<StaticTypeData> noDamageFrom = new HashSet<>();
 }
