b6560e897fdbe6aa185baf7b211bfca276bbc357
Luis Vallejos <vallejosevangelistaluis@gmail.com>
2025-11-23 11:04:12 -0500

    feat(config): agregar anotación @Order para priorizar configuración WebSocket
    - Se añadió la importación de Ordered y Order.
    - Se aplicó @Order(Ordered.HIGHEST_PRECEDENCE + 99) a la clase WebSocketConfig.
    - Se corrigió el comentario del autor en el Javadoc.

--- src/main/java/com/pokemon/game/config/WebSocketConfig.java
+++ src/main/java/com/pokemon/game/config/WebSocketConfig.java
@@ -1,6 +1,8 @@
 package com.pokemon.game.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -8,10 +10,11 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 /**
  *
- * @author Luis
+ * * @author Luis
  */
 @Configuration
 @EnableWebSocketMessageBroker
+@Order(Ordered.HIGHEST_PRECEDENCE + 99)
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
